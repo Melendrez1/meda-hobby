ae0a9d1af69bbb499862a8a4491cfa8a8158f65e
David Melendrez <melendrez.dc@gmail.com>
2018-06-20 23:09:17 +0000

    Adding html, js, css files.

---
+++ javasript.js
@@ -0,0 +1,43 @@
+console.log("something")
+
+var something = "Hello"
+
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday, Sunday"]
+
+var counter = 0;
+
+while (counter < length) {
+    
+    console.log (days[counter]);
+    
+    counter = counter + 1;
+}
+
+var favoritesFruit = ["Kiwi", "Apple", "Banana", "Dragonfruit", "Strawberry", "Blueberries"]
+
+console.log("My favorite fruits are:")
+
+var counter2 = 0
+while (counter2 < 10) {
+    counter2 = counter2 + 1;
+}
+
+for (var counter3 = 0; counter3 < 10, counter3 = counter3 + 1)
+
+var exampleArray = [1, 2, 3, 4];
+
+var exampleobject = { "First Number": 1, "Second Number": 2, "Third Number": 3}
+
+var user = {
+    "username": "egarcia";
+    "password": "123456";
+    "accountId" "000001";
+    "userage": 28,
+    "IsActive": true
+}
+
+function myFirstFunction() {}
+    console.log("Hi there:");
+    console.log("My name is:");
+    console.log("David");
+    
